2a3a37ad38d7959722b55144563235b3aa887fc3
shafamirazka <shafamiraz@gmail.com>
2024-10-23 07:59:04 +0700

    modified views.py

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306214025',
+        'name': 'Shafa Amira Azka',
+        'class': 'PBP B'
     }
 
     return render(request, "main.html", context)
