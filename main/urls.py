09f7ce8007bbaef9c814abeb71cab8a81ab39817
shafamirazka <shafamiraz@gmail.com>
2024-10-23 08:58:48 +0700

    nyoban

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from main.views import show_main, edit_review, delete_review
+from .views import show_main, create_review, edit_review, delete_review, list_reviews
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('edit/<int:pk>/', edit_review, name='edit_review'),
-    path('delete/<int:pk>/', delete_review, name='delete_review'),
+    path('reviews/', list_reviews, name='list_reviews'),
+    path('reviews/create/', create_review, name='create_review'),
+    path('reviews/edit/<int:id>/', edit_review, name='edit_review'),
+    path('reviews/delete/<int:id>/', delete_review, name='delete_review'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render
+# main/views.py
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ReviewForm
+from .models import Review
 
 def show_main(request):
-    context = {
-        'npm' : '2306214025',
-        'name': 'Shafa Amira Azka',
-        'class': 'PBP B'
-    }
+    return render(request, 'main/main.html')
 
-    return render(request, "main.html", context)
+def list_reviews(request):
+    reviews = Review.objects.all()
+    return render(request, 'main/review_list.html', {'reviews': reviews})
+
+def create_review(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main:list_reviews')
+    else:
+        form = ReviewForm()
+    return render(request, 'main/review_form.html', {'form': form})
+
+def edit_review(request, id):
+    review = get_object_or_404(Review, id=id)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('main:list_reviews')
+    else:
+        form = ReviewForm(instance=review)
+    return render(request, 'main/review_form.html', {'form': form})
+
+def delete_review(request, id):
+    review = get_object_or_404(Review, id=id)
+    if request.method == 'POST':
+        review.delete()
+        return redirect('main:list_reviews')
+    return render(request, 'main/review_confirm_delete.html', {'review': review})
