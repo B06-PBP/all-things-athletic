bf3ec491af78071aee75f4a45eb69c2005d9f1ec
shafamirazka <shafamiraz@gmail.com>
2024-10-23 15:55:34 +0700

    Resolved merge conflict in settings.py

--- all_things_athletic/settings.py
+++ all_things_athletic/settings.py
@@ -11,14 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-<<<<<<< HEAD
-import os
-=======
->>>>>>> origin/shafa
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
