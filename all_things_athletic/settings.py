eade56e22c491dcff8df5fed9aaaadccde144729
shafamirazka <shafamiraz@gmail.com>
2024-10-23 15:56:54 +0700

    Resolved merge conflict in settings.py

--- all_things_athletic/settings.py
+++ all_things_athletic/settings.py
@@ -118,9 +118,4 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-<<<<<<< HEAD
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-=======
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
->>>>>>> origin/shafa
