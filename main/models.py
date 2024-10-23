f3eba707384fa0636ffe48b48c11bebde62bc494
shafamirazka <shafamiraz@gmail.com>
2024-10-23 08:54:17 +0700

    nyoba

--- main/models.py
+++ main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class MoodEntry(models.Model):
-    mood = models.CharField(max_length=255)
-    time = models.DateField(auto_now_add=True)
-    feelings = models.TextField()
-    mood_intensity = models.IntegerField()
+class Review(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    rating = models.PositiveIntegerField()  # Rating dari 1 hingga 5
+    comment = models.TextField()  # Menyimpan banyak kalimat
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    @property
-    def is_mood_strong(self):
-        return self.mood_intensity > 5
+    def __str__(self):
+        return f'Review by {self.name} - Rating: {self.rating}'
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, edit_review, delete_review
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('edit/<int:pk>/', edit_review, name='edit_review'),
+    path('delete/<int:pk>/', delete_review, name='delete_review'),
 ]
